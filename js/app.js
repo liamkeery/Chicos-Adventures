a5b7341a8a69b2524b6c0522b6514eb704102dcf
liamkeery <liamkeery@gmail.com>
2019-04-04 18:23:59 -0400

    Home page update final

--- js/app.js
+++ js/app.js
@@ -5,3 +5,13 @@ $('.title-bar').on('sticky.zf.stuckto:top', function(){
 }).on('sticky.zf.unstuckfrom:top', function(){
   $(this).removeClass('shrink');
 })
+
+// closes the panel on click outside
+$(document).mouseup(function (e) {
+  var container = $('#contact-panel');
+  if (!container.is(e.target) // if the target of the click isn't the container...
+  && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+      container.removeClass('is-active');
+    }
+});
